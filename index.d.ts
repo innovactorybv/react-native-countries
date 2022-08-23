a31e842d5beb9c8fc9e1c436955f69bdc9bf6fdf
Yeshan Jay <yeshan@innovactory.com>
2022-08-23 13:02:36 +0200

    Modified type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,13 @@
-declare module "react-native-countries" {
-    export const getCountryNamesWithCodes: () => Promise<{ code: string; name: string }[]>
-    export const getCountryCodes: () => Promise<string[]>
-    export const getCountryNames: () => Promise<string[]>
+
+interface RNCountries {
+    getCountryNamesWithCodes: () => Promise<{ code: string; name: string }[]>
+    getCountryCodes: () => Promise<string[]>
+    getCountryNames: () => Promise<string[]>
 }
+declare const RNCountries: RNCountries
+
+export const getCountryNamesWithCodes: typeof RNCountries.getCountryNamesWithCodes
+export const getCountryCodes: typeof RNCountries.getCountryCodes
+export const getCountryNames: typeof RNCountries.getCountryNames
+
+export default RNCountries
